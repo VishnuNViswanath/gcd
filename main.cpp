490a9360c8d5e4a19ec38f367e87c144ed9b7a6c
redegg <2240797069@qq.com>
2017-06-03 17:47:45 +0800

    gcd
    gcd

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int gcd(int a,int b)
+{
+    if(b==0) return a;
+    return gcd(b,a%b);
+}
+
+int main()
+{
+    int x,y;
+    scanf("%d%d",&x,&y);
+    int g;
+    g=gcd(x,y);
+    printf("%d",g);
+    return 0;
+}
